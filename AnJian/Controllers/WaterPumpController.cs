4bd5404b36026ff25a51e682145de741a2981f88
YiNanan <zhangyan921129@outlook.com>
2018-07-19 11:43:18 +0800

    YiNanan
    新增水泵

---
+++ AnJian/Controllers/WaterPumpController.cs
@@ -0,0 +1,121 @@
+﻿using AnJian.Models.WaterPump;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnJian.Controllers
+{
+    public class WaterPumpController : Controller
+    {
+        // GET: WaterPump
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// 分支水泵
+        /// </summary>
+        /// <param name="parm"></param>
+        /// <returns></returns>
+        public ActionResult BranchWaterPumpRunTime(string parm)
+        {
+
+            ViewBag.V = "Kv";
+            ViewBag.T = " ℃";
+            WatrerPumpParms wp;
+            switch (parm)
+            {
+                case "1":
+                    ViewBag.Title = "一号水泵-运行参数";
+                    wp = new WatrerPumpParms();
+                    ViewBag.motorVoltsge = wp.motorVoltsge;
+                    ViewBag.motorTemperature = wp.motorTemperature;
+                    ViewBag.frontTemperature = wp.frontTemperature;
+                    ViewBag.rearTemperature = wp.rearTemperature;
+                    ViewBag.statorTemperature = wp.statorTemperature;
+                    break;
+                case "2":
+                    ViewBag.Title = "二号水泵-运行参数";
+                    wp = new WatrerPumpParms();
+                    ViewBag.motorVoltsge = wp.motorVoltsge;
+                    ViewBag.motorTemperature = wp.motorTemperature;
+                    ViewBag.frontTemperature = wp.frontTemperature;
+                    ViewBag.rearTemperature = wp.rearTemperature;
+                    ViewBag.statorTemperature = wp.statorTemperature;
+                    break;
+                case "3":
+                    ViewBag.Title = "三号水泵-运行参数";
+                    wp = new WatrerPumpParms();
+                    ViewBag.motorVoltsge = wp.motorVoltsge;
+                    ViewBag.motorTemperature = wp.motorTemperature;
+                    ViewBag.frontTemperature = wp.frontTemperature;
+                    ViewBag.rearTemperature = wp.rearTemperature;
+                    ViewBag.statorTemperature = wp.statorTemperature;
+                    break;
+                default:
+                    ViewBag.Title = "未知错误";
+                    wp = new WatrerPumpParms();
+                    ViewBag.motorVoltsge = wp.motorVoltsge;
+                    ViewBag.motorTemperature = wp.motorTemperature;
+                    ViewBag.frontTemperature = wp.frontTemperature;
+                    ViewBag.rearTemperature = wp.rearTemperature;
+                    ViewBag.statorTemperature = wp.statorTemperature;
+                    break;
+
+            }
+            return View();
+        }
+
+        /// <summary>
+        /// 中心水泵
+        /// </summary>
+        /// <param name="parm"></param>
+        /// <returns></returns>
+        public ActionResult CenterWaterPumpRunTime()
+        {
+            ViewBag.H = "m";
+            ViewBag.F = " m³/h";
+            ViewBag.Title = "中央水泵-运行参数";
+            WatrerPumpParms wp = new WatrerPumpParms();
+            ViewBag.sumpLevel = wp.sumpLevel;
+            ViewBag.mainFlow = wp.mainFlow;
+            ViewBag.sublineFlow = wp.sublineFlow;
+            return View();
+        }
+
+        /// <summary>
+        /// 控制柜
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ControlCabinetRunTime()
+        {
+            Random ran = new Random();
+            ViewBag.Title = "控制柜-运行状态";
+            //远控提示
+            ViewBag.remoteControl = ran.Next(0, 2) == 0 ? true : false;
+            //集控提示
+            ViewBag.centralzedControl = ran.Next(0, 2) == 0 ? true : false;
+            //手动提示
+            ViewBag.manualControl = ran.Next(0, 2) == 0 ? true : false;
+            //自动提示
+            ViewBag.autoControl = ran.Next(0, 2) == 0 ? true : false;
+            //主管路提示
+            ViewBag.mainFlowControl = ran.Next(0, 2) == 0 ? true : false;
+            //备管路提示
+            ViewBag.sublineFlowControl = ran.Next(0, 2) == 0 ? true : false;
+            return View();
+        }
+
+        public ActionResult Water()
+        {
+            //主页面
+            return View();
+        }
+
+
+    }
+}
---
+++ AnJian/Models/WaterPump/WatrerPumpParms.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace AnJian.Models.WaterPump
+{
+    public class WatrerPumpParms
+    {
+        static Random ran = new Random();
+
+        [DisplayName("水仓液位")]
+        public string sumpLevel { get; set; } = (ran.Next(300, 400) * 0.01).ToString(".00");
+        [DisplayName("主管路流量")]
+        public string mainFlow { get; set; } = (ran.Next(20000, 40000) * 0.01).ToString(".00");
+        [DisplayName("备用管路流量")]
+        public string sublineFlow { get; set; } = (ran.Next(20000, 40000) * 0.01).ToString(".00");
+        [DisplayName("电机电压")]
+        public string motorVoltsge { get; set; } = (ran.Next(300, 500) * 0.01).ToString(".00");
+        [DisplayName("电机温度")]
+        public string motorTemperature { get; set; } = (ran.Next(5000, 7000) * 0.01).ToString(".00");
+        [DisplayName("前轴温度")]
+        public string frontTemperature { get; set; } = (ran.Next(5000, 7000) * 0.01).ToString(".00");
+        [DisplayName("后轴温度")]
+        public string rearTemperature { get; set; } = (ran.Next(5000, 7000) * 0.01).ToString(".00");
+        [DisplayName("定子温度")]
+        public string statorTemperature { get; set; } = (ran.Next(5000, 7000) * 0.01).ToString(".00");
+    }
+}
