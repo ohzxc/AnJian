e589594a937670e76d740ba875931a6d9cd83fd9
zxc <zhouxc@qq.com>
2018-07-23 09:25:21 +0800

    添加控制器

---
+++ AnJian/Controllers/DataCenterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnJian.Controllers
+{
+    public class DataCenterController : Controller
+    {
+        // GET: DataCenter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
